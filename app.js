b50fdbcefa72e2e30432093e054d8969c9828f94
Zheng Hanming <hanming2033@gmail.com>
2018-02-17 18:21:06 +0800

    add the rest of the logics and bind the events

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //Item Controller for local data
 const CtrlItem = (() => {
   //meal constructor
-  const meal = function (id, name, calories) {
+  const Meal = function (id, name, calories) {
     this.id = id;
     this.name = name;
     this.calories = calories;
@@ -54,17 +54,31 @@ const CtrlItem = (() => {
       //JSON.parse(JSON.stringify(obj)) is a way to deep copy a obj's variables but not methods
       return JSON.parse(JSON.stringify(data))
     },
-    setCurrent: (id) => data.currentItem = id,
     addMeal: (name, calories) => {
       const id = Date.now();
-      const mealTmp = new meal(id, name, calories);
+      const mealTmp = new Meal(id, name, calories);
       addData(mealTmp);
     },
     removeMeal: (id) => {
       removeData(id);
+      data.currentItem = null;
     },
     updateMeal: (id, name, calories) => {
       updateData(id, name, calories);
+      data.currentItem = null;
+    },
+    toEditItem: (id) => {
+      const meals = data.items;
+      const meal = meals.find((meal) => {
+        return meal.id == id;
+      })
+      data.currentItem = meal;
+    },
+    cancelEditItem: () => {
+      data.currentItem = null;
+    },
+    clearAllMeals: () => {
+      localStorage.clear();
     }
   }
 })();
@@ -78,9 +92,11 @@ const CtrlUI = (() => {
     btnUpdate: document.querySelector('.update-btn'),
     btnDelete: document.querySelector('.delete-btn'),
     btnBack: document.querySelector('.back-btn'),
+    btnClear: document.querySelector('.clear-btn'),
     inputMeal: document.querySelector('#item-name'),
     inputCalories: document.querySelector('#item-calories'),
-    totalCal: document.querySelector('.total-calories')
+    totalCal: document.querySelector('.total-calories'),
+    instruction: document.querySelector('.card-title')
   }
 
   //PRIVATE METHODS
@@ -120,7 +136,7 @@ const CtrlUI = (() => {
       UISelector.btnUpdate.style.display = 'inline-block';
       UISelector.btnDelete.style.display = 'inline-block';
       UISelector.btnBack.style.display = 'inline-block';
-    }  else {
+    } else {
       UISelector.btnAdd.style.display = 'inline-block';
       UISelector.btnUpdate.style.display = 'none';
       UISelector.btnDelete.style.display = 'none';
@@ -133,8 +149,13 @@ const CtrlUI = (() => {
     if (meal !== null) {
       UISelector.inputMeal.value = meal.name
       UISelector.inputCalories.value = meal.calories
+      UISelector.instruction.textContent = 'Edit/Delete Item'
+    } else {
+      UISelector.instruction.textContent = 'Add Meal / Food Item'
     }
+    UISelector.inputMeal.focus();
   }
+
   //PUBLIC METHODS
   return {
     renderUI: (state) => {
@@ -149,6 +170,12 @@ const CtrlUI = (() => {
         name: UISelector.inputMeal.value,
         calories: parseInt(UISelector.inputCalories.value)
       }
+    },
+    getCurrentItemId: function (e) {
+      if (e.target.classList.contains('edit-item')) {
+        const target = e.target.parentElement.parentElement;
+        return target.id.substr(5);
+      };
     },
     clearInput: () => {
       UISelector.inputMeal.value = '';
@@ -165,22 +192,71 @@ const App = ((CtrlItem, CtrlUI) => {
   const syncData = () => {
     //get state info from CtrlItem
     const state = CtrlItem.getState();
-    console.log(state.currentItem);
-    state.currentItem = {id: 123234098395, name: 'Sweet and Sour Pork', calories: 938}
-    console.log(state.currentItem);
+    //state.currentItem = { id: 123234098395, name: 'Sweet and Sour Pork', calories: 938 }
     CtrlUI.renderUI(state);
   }
 
   //bind events
   const eventBinding = () => {
     const uiSelector = CtrlUI.getUiSelector();
+    //event action: add new item
     uiSelector.btnAdd.addEventListener('click', addItem);
+    //event action: get item from edit button
+    uiSelector.itemList.addEventListener('click', getCurrentItem);
+    //event action: cancel editing an item
+    uiSelector.btnBack.addEventListener('click', cancelEdit)
+    //event action: update meal
+    uiSelector.btnUpdate.addEventListener('click', updateItem)
+    //event action: delete meal
+    uiSelector.btnDelete.addEventListener('click', deleteItem)
+    //event action: clear all meals
+    uiSelector.btnClear.addEventListener('click', clearItems)
+
   }
 
   //event action: add new item
   const addItem = function (e) {
     const meal = CtrlUI.getInput();
     if (meal != null) CtrlItem.addMeal(meal.name, meal.calories);
+    CtrlUI.clearInput();
+    syncData();
+    e.preventDefault();
+  }
+  //event action: get item from edit button
+  const getCurrentItem = (e) => {
+    const mealId = CtrlUI.getCurrentItemId(e);
+    if (mealId === undefined) return;
+    CtrlItem.toEditItem(mealId);
+    syncData();
+    e.preventDefault();
+  }
+  //event action: cancel editing an item
+  const cancelEdit = () => {
+    CtrlItem.cancelEditItem();
+    CtrlUI.clearInput();
+    syncData();
+    e.preventDefault();
+  }
+  //event action: update meal
+  const updateItem = (e) => {
+    const input = CtrlUI.getInput();
+    const currentItem = CtrlItem.getState().currentItem;
+    CtrlItem.updateMeal(currentItem.id, input.name, input.calories);
+    CtrlUI.clearInput();
+    syncData();
+    e.preventDefault();
+  }
+  //event action: delete meal
+  const deleteItem = (e) => {
+    const currentItem = CtrlItem.getState().currentItem;
+    CtrlItem.removeMeal(currentItem.id);
+    CtrlUI.clearInput();
+    syncData();
+    e.preventDefault();
+  }
+  //event action: clear all meals 
+  const clearItems = () => {
+    CtrlItem.clearAllMeals();
     CtrlUI.clearInput();
     syncData();
     e.preventDefault();
